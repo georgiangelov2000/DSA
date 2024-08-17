0d451f3ad08e44dc584a1f4eb32f9d00cad73116
georgiangelov2000 <gangelov2000@gmail.com>
2024-08-17 17:27:18 +0300

    binary-seach-for-array

---
+++ Heaps-BST/BinarySearchInArray.php
@@ -0,0 +1,102 @@
+<?php
+// Iterative Binary Search log(n)
+//Auxiliary Space: O(log(n)) due to recursive calls, otherwise iterative version uses Auxiliary Space of O(1).
+function binarySearch($arr, $low, 
+                      $high, $x)
+{
+    while ($low <= $high)
+    {
+        $mid = floor($low + ($high - $low) / 2);
+
+        // Check if x is present at mid
+        if ($arr[$mid] == $x)
+            return floor($mid);
+
+        // If x greater, ignore
+        // left half
+        if ($arr[$mid] < $x)
+            $low = $mid + 1;
+
+        // If x is smaller, 
+        // ignore right half
+        else
+            $high = $mid - 1;
+    }
+
+    // If we reach here, then 
+    // element was not present
+    return -1;
+}
+
+//Time Complexity: O(N) [In the worst case all elements may have to be moved] 
+//Auxiliary Space: O(1)
+function insertedSorted($arr,$n,$key) {
+    for ($i = $n-1; $i >= 0 && $arr[$i] > $key; $i--)
+    {
+        $arr[$i+1] = $arr[$i];
+    }
+    
+    $arr[$i+1] = $key;
+    return ($n+1);
+}
+
+//Time Complexity: O(log(n)) Using Binary Search
+function binarySearchRecursively($arr,$low,$high,$key){
+    if($high < $low ) {
+        return -1;
+    }
+    $mid = floor(($low + $high) / 2);
+    if($arr[$mid] == $key) {
+        return $mid;
+    }
+    if($arr[$mid] < $key) {
+        return binarySearchRecursively($arr,$mid+1,$high,$key);
+    }
+    else {
+        return binarySearchRecursively($arr,$low,$mid-1,$key);
+    }
+}
+
+//Time Complexity: O(N). In the worst case all elements may have to be moved
+//Auxiliary Space: O(log N). An implicit stack will be use
+function deleteElement($arr,$n,$key) {
+    $pos = binarySearchRecursively($arr, 0, $n - 1, $key);
+
+    if(!$pos) {
+        echo "Element not found";
+        return $n;
+    }
+    $i = null;
+    for ($i = $pos; $i < $n - 1; $i++) { 
+        $arr[$i] = $arr[$i + 1];
+    }
+
+    $n = $n - 1;
+    unset($arr[$n]);
+
+    return $n;
+}
+
+
+// Deletion Binary Search tree Recursively
+$arr = array(12,16,20,40,50,70);
+$n = count($arr);
+$x = 16;
+deleteElement($arr,$n,$x);
+
+// Insertion Binary Search tree
+// $arr = array(12,16,20,40,50,70);
+// $n = count($arr);
+// $x = 25;
+// insertedSorted($arr,$n,$x);
+
+// Binary search iterative
+// $arr = array(2, 3, 4, 10, 40);
+// $n = count($arr);
+// $x = 10;
+// $result = binarySearch($arr, 0, 
+//                        $n - 1, $x);
+// if(($result == -1))
+// echo "Element is not present in array";
+// else
+// echo "Element is present at index ";
--- Heaps-BST/BinarySearchTree.php
+++ Heaps-BST/BinarySearchTree.php
@@ -1,131 +1,57 @@
 <?php
-
-class Node 
+function binarySearch($a, $item, $low, $high)
 {
-  public $key;
-  public $left;
-  public $right;
-
-  public function __construct($key) {
-    $this->key = $key;
-    $this->left = null;
-    $this->right = null;
-  }
-
-  // function to search a key in a BST
-    public function search($root, $key) {
-        if ($root == null || $root->key === $key) {
-            return $root;
-        }
-
-        // Key is greater than root's key
-        if($root->key < $key) {
-            return $this->search($root->right, $key);
-        }
-
-        // Key is smaller than root's key
-        return $this->search($root->left, $key);
-    }
-
-    // If the tree is empty, return a new node
-    public function insert($root, $key){
-        if($root == null){
-            $root = new Node($key);
-            return $root;
-        }
-
-        if($root->key == $key) {
-            return $root;
-        }
-
-        if($root->key < $key) {
-            $root->right = $this->insert($root->right, $key);
-        } else {
-            $root->left = $this->insert($root->left, $key);
-        }
-
-        return $root;
-    }
-
-    // This function deletes a given key x from the 
-    // given BST  and returns the modified root of 
-    // the BST (if it is modified).
-    public function delete($root,$x) {
-        // Base case
-        if($root == null) {
-            return $root;
-        }
-
-        // If key to be searched is in a subtree
-        if($root->key > $x) {
-            $root->left = $this->delete($root->left, $x);
-        } else if($root->key < $x) {
-            $root->right = $this->delete($root->right, $x);
-        } else {
-            // If root matches with the given key
-
-            // Cases when root has 0 children or
-            // only right child
-            if($root->left == null) {
-                return $root->right;
-            }
-
-            // When root has only left child
-            if($root->right == null) {
-                return $root->left;
-            }
-
-            // When both children are present
-            $succ = $this->getSuccessor($root);
-            $root->key = $succ->key;
-            $root->right = $this->delete($root->right, $succ->key);
-        }
-        return $root;
-    }
-
-    // Note that it is not a generic inorder successor 
-    // function. It mainly works when the right child
-    // is not empty, which is the case we need in BST
-    // delete. 
-    public function getSuccessor($curr) {
-        $succ = $curr->right;
-        while($succ != null && $succ->left!= null) {
-            $succ = $succ->left;
-        }
-        return $succ;
-    }
-
-    public function inOrder($root) {
-        if($root != null) {
-            $this->inOrder($root->left);
-            echo $root->key." ";
-            $this->inOrder($root->right);
-        }
-    }
-}
-
-// $root = new Node(50);
-// $root->left = new Node(30);
-// $root->right = new Node(70);
-// $root->left->left = new Node(20);
-// $root->left->right = new Node(40);
-// $root->right->left = new Node(60);
-// $root->right->right = new Node(80);
-
-// // Searching for keys in the BST
-// echo $root->search($root, 19) !== null ? "Found"
-//                                       : "Not Found";
-// echo $root->search($root, 80) !== null ? "Found"
-//                                       : "Not Found";
-
-$root = new Node(10);
-$root->insert($root,40);
-$root->insert($root,60);
-$root->insert($root,30);
-$root->insert($root,20);
-$root->insert($root,5);
-$root->insert($root,6);
-$root->insert($root,1);
-$root->insert($root,2);
-$root->delete($root, 40);
-$root->inOrder($root);
+ 
+    if ($high <= $low) 
+        return ($item > $a[$low]) ? 
+                       ($low + 1) : $low; 
+ 
+    $mid = (int)(($low + $high) / 2); 
+ 
+    if($item == $a[$mid]) 
+        return $mid + 1; 
+ 
+    if($item > $a[$mid]) 
+        return binarySearch($a, $item, 
+                            $mid + 1, $high); 
+         
+    return binarySearch($a, $item, $low, 
+                            $mid - 1); 
+} 
+ 
+// Function to sort an array a of size 'n' 
+function insertionSort(&$a, $n) 
+{ 
+    $i = null; $loc = null; $j = null; $k = null; $selected = null; 
+ 
+    for ($i = 1; $i < $n; ++$i) 
+    { 
+        $j = $i - 1; 
+        $selected = $a[$i]; 
+ 
+        // find location where selected 
+        // item should be inserted 
+        $loc = binarySearch($a, $selected, 0, $j); 
+ 
+        // Move all elements after location 
+        // to create space 
+        while ($j >= $loc) 
+        { 
+            $a[$j + 1] = $a[$j]; 
+            $j--; 
+        } 
+        $a[$j + 1] = $selected; 
+    } 
+} 
+ 
+// Driver Code
+$a = array(37, 23, 0, 17, 12, 72, 
+           31, 46, 100, 88, 54); 
+            
+$n = sizeof($a); 
+ 
+insertionSort($a, $n); 
+ 
+echo "Sorted array:\n"; 
+for ($i = 0; $i < $n; $i++) 
+    echo "$a[$i] "; 

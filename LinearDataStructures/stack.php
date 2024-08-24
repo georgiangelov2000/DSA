d537705c502029d275cb192c6227c6ed58acb5e1
georgiangelov2000 <gangelov2000@gmail.com>
2024-08-24 13:20:14 +0300

    stack.php

--- LinearDataStructures/stack.php
+++ LinearDataStructures/stack.php
@@ -39,6 +39,22 @@ class Stack {
         }
         echo "\n";
     }
+
+    public function search($element) {
+        for ($i = $this->size - 1; $i >= 0; $i--) {
+            if ($this->stack[$i] == $element) {
+                return $this->size - $i - 1; // Връща разстоянието от върха на стека
+            }
+        }
+        return -1; // Елементът не е намерен
+    }
+
+    public function access($index) {
+        if ($index < 0 || $index >= $this->size) {
+            return "Невалиден индекс";
+        }
+        return $this->stack[$this->size - $index - 1];
+    }
 }
 
 $stack = new Stack();
@@ -49,7 +65,7 @@ $stack->push(3);
 
 echo "Елементите в стека: ";
 $stack->display();
-
+echo "Намерен елемент: " . $stack->search(1);
 echo "Върхът на стека е: " . $stack->peek() . "\n";
 
 echo "Изваден елемент: " . $stack->pop() . "\n";
@@ -57,3 +73,4 @@ echo "Изваден елемент: " . $stack->pop() . "\n";
 
 echo "Елементите в стека след изваждане: ";
 $stack->display();
+echo "Елементът на позиция 0 от върха на стека е: " . $stack->access(0) . "\n";
